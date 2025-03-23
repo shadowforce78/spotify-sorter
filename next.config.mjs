f243bc9305dc23a996ecc894ae06d1a0def70950
shadowforce78 <planque.adam@gmail.com>
2025-03-23 22:40:52 +0100

    Add new files and configurations for various modules, remove jsconfig.json and vercel.svg

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['storage.googleapis.com','i.stack.imgur.com'],
-    },
-    reactStrictMode: true,
-};
-
-export default nextConfig;
